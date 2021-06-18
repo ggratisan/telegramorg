7d8edc7069772a9b86408dc70dd3a3fe1cbb95a3
ggratisan <83497870+ggratisan@users.noreply.github.com>
2021-06-18 10:29:47 +0800

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,19 @@
+class Translation(object):
+    START_TEXT = """Hi!
+Terimakasih telah menggunakanku😬
+Masukkan nomormu ( +62 )
+
+/start jika salah masukkan detail"""
+    AFTER_RECVD_CODE_TEXT = """ok!
+Masukkan kode dari telegram!
+
+Kode ini hanya untuk mendapatkan info.
+Jika kamu tidak percaya dengan publisher bot ini,
+Silahkan deploy sendiri di https://github.com/ggratisan/telegramorg
+
+/start jika salah masukkan detail"""
+    BEFORE_SUCC_LOGIN = "Diterima, mendapatkan detail ..."
+    ERRED_PAGE = "eror. \n\n@derylchat"
+    CANCELLED_MESG = "Dah! /start"
+    IN_VALID_CODE_PVDED = "Maaf, kode salah"
+    IN_VALID_PHNO_PVDED = "Maaf, nomor salah"
